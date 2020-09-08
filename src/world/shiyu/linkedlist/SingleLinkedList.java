12741abc0739abb1ee764fcf62413e15dbd8b3ae
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-09-08 03:21:35 -0700

    feat: reversedList() 百度

--- src/world/shiyu/linkedlist/SingleLinkedList.java
+++ src/world/shiyu/linkedlist/SingleLinkedList.java
@@ -1,10 +1,10 @@
 package world.shiyu.linkedlist;
 
 
-class SingleLikedList{
+class SingleLikedList {
     private HeroNode head = new HeroNode(0, "", ""); // 初始化头节点， 不存在具体数据
 
-    public HeroNode getHead(){
+    public HeroNode getHead() {
         return head;
     }
 
@@ -12,12 +12,12 @@ class SingleLikedList{
     // 思路， 当不考虑编号顺序时。
     // 1， 找到当前链表当最后一个节点，
     // 2， 将最后这个节点当next指向新当节点
-    public void add(HeroNode heroNode){
+    public void add(HeroNode heroNode) {
         // 因为head节点不能动， 因此文明需要一个辅助变量temp
         HeroNode temp = this.head;
         // 遍历链表， 找到最后
-        while(true){
-            if(temp.next == null){
+        while (true) {
+            if (temp.next == null) {
                 break;
             }
             // 如果不是最后一个节点， temp便后移
@@ -27,18 +27,18 @@ class SingleLikedList{
         temp.next = heroNode;
     }
 
-    public void addByOrder(HeroNode heroNode){
+    public void addByOrder(HeroNode heroNode) {
         // 因为头节点不能动， 依然通过辅助指针（变量）来帮助找到加入位置
         // temp位于添加位置动前一个节点
         HeroNode temp = head;
         boolean flag = false; // 标示添加编号是否存在， 默认为false
-        while(true){
-            if(temp.next == null){ // 说明temp已经在链表最后
+        while (true) {
+            if (temp.next == null) { // 说明temp已经在链表最后
                 break;
             }
-            if(temp.next.no > heroNode.no){ // target位置找到， 后面一位index为target
+            if (temp.next.no > heroNode.no) { // target位置找到， 后面一位index为target
                 break;
-            }else if(temp.next.no == heroNode.no){ // 编号已经存在
+            } else if (temp.next.no == heroNode.no) { // 编号已经存在
                 flag = true;
                 break;
             }
@@ -46,52 +46,52 @@ class SingleLikedList{
         }
 
         // 判断flag值
-        if(flag){
-            System.out.println("已存在编号: "+ heroNode.no);
-        }else {
+        if (flag) {
+            System.out.println("已存在编号: " + heroNode.no);
+        } else {
             // 插入链表, temp后面
             heroNode.next = temp.next;
             temp.next = heroNode;
         }
     }
 
-    public void update(HeroNode newHeroNode){
-        if(head.next == null){
+    public void update(HeroNode newHeroNode) {
+        if (head.next == null) {
             System.out.println("链表为空");
             return;
         }
         // 找到需要修改动节点， 根据编号
         HeroNode temp = head.next;
         boolean flag = false;
-        while(true){
-            if(temp == null){ // 链表最后节点， 遍历完毕
+        while (true) {
+            if (temp == null) { // 链表最后节点， 遍历完毕
                 break;
             }
-            if(temp.no == newHeroNode.no){ // 目标节点
+            if (temp.no == newHeroNode.no) { // 目标节点
                 flag = true;
                 break;
             }
             temp = temp.next;
         }
         // 根据flag判断是否找到目标节点
-        if(flag){
+        if (flag) {
             temp.name = newHeroNode.name;
             temp.nickName = newHeroNode.nickName;
-        }else {
+        } else {
             System.out.printf("没有找到编号 %d 的节点， 不能修改\n", newHeroNode.no);
         }
     }
 
-    public void list(){
+    public void list() {
         // 判断链表是否为空
-        if(head.next == null) {
+        if (head.next == null) {
             System.out.println("链表为空");
             return;
         }
         // 因为头节点不可动， 因此我们需要一个辅助变量来遍历
         HeroNode temp = head.next;
-        while(true){
-            if(temp == null){
+        while (true) {
+            if (temp == null) {
                 break;
             }
             System.out.println(temp);
@@ -100,41 +100,41 @@ class SingleLikedList{
     }
 
     /**
-     *  删除节点
-     *  1， 先找到需要删除的节点
-     *  2， temp.next = temp.next.next;
-     *  3, 被删除的节点， 将不会有其他任何引用指向， 会被jvm垃圾回收机制回收
-     * */
-    public void del(int no){
+     * 删除节点
+     * 1， 先找到需要删除的节点
+     * 2， temp.next = temp.next.next;
+     * 3, 被删除的节点， 将不会有其他任何引用指向， 会被jvm垃圾回收机制回收
+     */
+    public void del(int no) {
         HeroNode temp = head;
         boolean flag = false; // 标示是否找到待删除节点
-        while(true){
-            if(temp.next == null){ // 链表最后节点
+        while (true) {
+            if (temp.next == null) { // 链表最后节点
                 break;
             }
-            if(temp.next.no == no){ // 目标删除节点的前一个节点temp
+            if (temp.next.no == no) { // 目标删除节点的前一个节点temp
                 flag = true;
                 break;
             }
             temp = temp.next;
         }
 
-        if(flag){
+        if (flag) {
             temp.next = temp.next.next;
-        }else {
+        } else {
             System.out.printf("要删除的%d节点不存在\n", no);
         }
     }
 }
 
 // 定义节点， 每个节点对象为一个节点
-class HeroNode{
+class HeroNode {
     public int no;
     public String name;
     public String nickName;
     public HeroNode next;
 
-    public HeroNode(int no, String name, String nickName){
+    public HeroNode(int no, String name, String nickName) {
         this.no = no;
         this.name = name;
         this.nickName = nickName;
--- src/world/shiyu/linkedlist/SingleLinkedListDemo.java
+++ src/world/shiyu/linkedlist/SingleLinkedListDemo.java
@@ -1,13 +1,13 @@
 package world.shiyu.linkedlist;
 
 public class SingleLinkedListDemo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         HeroNode hero_1 = new HeroNode(1, "宋江", "及时雨");
         HeroNode hero_2 = new HeroNode(2, "卢俊义", "玉麒麟");
         HeroNode hero_3 = new HeroNode(3, "吴用", "智多星");
         HeroNode hero_4 = new HeroNode(4, "林冲", "豹子头");
 
-        SingleLikedList singleLinkedList= new SingleLikedList();
+        SingleLikedList singleLinkedList = new SingleLikedList();
         singleLinkedList.add(hero_1);
         singleLinkedList.add(hero_2);
         singleLinkedList.add(hero_3);
@@ -20,46 +20,75 @@ public class SingleLinkedListDemo {
         System.out.println("删除后链表情况");
         singleLinkedList.list();
 
-        System.out.println("有效的节点个数= "+getLength(singleLinkedList.getHead()));
+        System.out.println("有效的节点个数= " + getLength(singleLinkedList.getHead()));
 
     }
 
 
-    /** 查找单链表中的倒数第K个节点 （新浪）
-     *  1， 编写一个方法， 接受head节点， 同时接受一个index
-     *  2， index标示是倒数第index节点
-     *  3， 先把链表从头到尾遍历， 得到链表的总长度getLength()
-     *  4， 得到size后， 我们从链表的第一个开始遍历（size - index）个
-     *  5,  如果找到即返回该节点， 否则返回null
-     * */
-    public static HeroNode findInverseIndexNode(HeroNode head, int index){
-        if(head.next == null){
+    /**
+     * 查找单链表中的倒数第K个节点 （新浪）
+     * 1， 编写一个方法， 接受head节点， 同时接受一个index
+     * 2， index标示是倒数第index节点
+     * 3， 先把链表从头到尾遍历， 得到链表的总长度getLength()
+     * 4， 得到size后， 我们从链表的第一个开始遍历（size - index）个
+     * 5,  如果找到即返回该节点， 否则返回null
+     */
+    public static HeroNode findInverseIndexNode(HeroNode head, int index) {
+        if (head.next == null) {
             return null;
         }
         // 第一次遍历得到链表的长度
         int size = getLength(head);
         // 第二次遍历size-index位置，为我们的倒数第K个节点
-        if(index <=0  || index > size){
+        if (index <= 0 || index > size) {
             return null;
         }
         HeroNode cur = head.next;
-        for(int i=0; i<size-index; i++){
+        for (int i = 0; i < size - index; i++) {
             cur = cur.next;
         }
         return cur;
     }
 
     // 方法： 获取到单链表到节点个数（如果是带头节点的链表， 需求不统计头节点）（百度）
-    public static int getLength(HeroNode head){
-        if(head.next == null){
+    public static int getLength(HeroNode head) {
+        if (head.next == null) {
             return 0;
         }
         int length = 0;
         HeroNode cur = head.next;
-        while(cur != null){
-            length ++;
+        while (cur != null) {
+            length++;
             cur = cur.next;
         }
         return length;
+    }
+
+    /**
+     * 反转单链表 （腾讯）
+     * 思路：
+     * 1， 先定义一个节点reverseHead = new HeroNode();
+     * 2, 从头到尾遍历原来单链表， 每遍历一个节点， 就将其取出， 并放在新单链表reverseHead最前端。
+     * 3, 原来单链表单head.next = reverseHead.next
+     */
+    public static void reverseList(HeroNode head) {
+        // 如果当前列表为空， 或者只有一个节点， 则无需反转，直接返回
+        if (head.next == null || head.next.next == null) {
+            return;
+        }
+
+        // 定义一个辅助变量指针， 帮助我们遍历原来单链表
+        HeroNode curr = head.next;
+        HeroNode next = null; // 指向当前节点【cur】下一个节点
+        HeroNode reversedHead = new HeroNode(0, "", "");
+        // 遍历原来单链表， 并从头到尾遍历原来单链表， 每遍历一个节点， 就将其取出， 并放在新单链表reverseHead最前端。
+        while (curr != null) {
+            next = curr.next; // 先暂时保存当前节点单下一个节点， 因为后面需要使用
+            curr.next = reversedHead.next; // 将curr的下一个节点指向新链表的最前端
+            reversedHead.next = curr; // 拼接reverseHead与curr， 成为新链表（reversed）
+            curr = next; // 移动后一次继续进行遍历
+        }
+        //  将head.next指向reverseHead.next, 实现单链表单反转
+        head.next = reversedHead.next;
     }
 }
