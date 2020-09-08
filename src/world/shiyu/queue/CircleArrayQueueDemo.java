4f8981a89bd381d1767037217f3aef5a133035a9
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-09-07 19:47:30 -0700

    feat CircleArrayQueueDemo

--- src/world/shiyu/queue/CircleArrayQueueDemo.java
+++ src/world/shiyu/queue/CircleArrayQueueDemo.java
@@ -2,13 +2,14 @@ package world.shiyu.queue;
 
 import java.util.Scanner;
 
-public class ArrayQueueDemo {
+@SuppressWarnings("Duplicates")
+public class CircleArrayQueueDemo {
     public static void main(String[] args){
-        ArrayQueue arrayQueue = new ArrayQueue(3);
+        CircleArrayQueue circleArrayQueue = new CircleArrayQueue(4); // 其队列有效数据最多为3
         char key;
         Scanner scanner = new Scanner(System.in);
         boolean loop = true;
-        System.out.println("System online. test: ArrayQueueDemo");
+        System.out.println("System online. test: CircleArrayQueueDemo");
 
         while(loop){
             System.out.println("s(show): show the queue");
@@ -19,20 +20,20 @@ public class ArrayQueueDemo {
             key = scanner.next().charAt(0);
             switch (key){
                 case 's':
-                    arrayQueue.showQueue();
+                    circleArrayQueue.showQueue();
                     break;
                 case 'a':
                     try {
                         System.out.print("Please type a number: ");
                         int value = scanner.nextInt();
-                        arrayQueue.addQueue(value);
+                        circleArrayQueue.addQueue(value);
                     }catch (Exception e){
                         System.out.println(e.getMessage());
                     }
                     break;
                 case 'g':
                     try {
-                        int result = arrayQueue.getQueue();
+                        int result = circleArrayQueue.getQueue();
                         System.out.printf("You just got the data: %d\n", result);
                     }catch (Exception e){
                         System.out.println(e.getMessage());
@@ -40,8 +41,8 @@ public class ArrayQueueDemo {
                     break;
                 case 'h':
                     try{
-                        int result = arrayQueue.peekqueue();
-                       System.out.printf("You just peaked the data head: %d\n", result);
+                        int result = circleArrayQueue.peekqueue();
+                        System.out.printf("You just peaked the data head: %d\n", result);
                     }catch (Exception e){
                         System.out.println(e.getMessage());
                     }
@@ -60,21 +61,21 @@ public class ArrayQueueDemo {
     }
 }
 
-class ArrayQueue{
+class CircleArrayQueue{
     private int maxSize;
-    private int front;
-    private int rear;
+    private int front; // front 指向队列的第一个元素， 也就是说arr[front], 初始值为0.
+    private int rear; // rear指向队列最后一个元素的后一个位置， 因为希望空出一个空间最为约定， 初始值为0.
     private int[] arr;
 
-    public ArrayQueue(int arrMaxSize){
+    public  CircleArrayQueue(int arrMaxSize){
         maxSize = arrMaxSize;
         arr = new int[maxSize];
-        front = -1; // ahead one index of queue head
-        rear = -1; // queue tail, the last index of the queue
+        front = 0;
+        rear = 0;
     }
 
     public boolean isFull(){
-        return rear == maxSize - 1;
+        return (rear + 1) % maxSize == front;
     }
 
     public boolean isEmpty(){
@@ -86,8 +87,8 @@ class ArrayQueue{
         if(isFull()){
             throw new RuntimeException("The queue is full!");
         }
-        rear++; // move rear with one index behind
         arr[rear] = n;
+        rear = (rear + 1) % maxSize; // 将rear后移， 这里必须考虑取模, 这样子保证了不越界
     }
 
     public int getQueue(){
@@ -95,24 +96,38 @@ class ArrayQueue{
         if(isEmpty()){
             throw new RuntimeException("The queue is empty!");
         }
-        front++;
-        return arr[front];
+        // 这里需要分析front是指向队列的第一个元素
+        // 1，先把front的对应的值保存给临时变量
+        // 2，再将front后移, 考虑取摸
+        // 3，将临时保存的变量返回
+        int value = arr[front];
+        front = (front + 1) % maxSize;
+        return value;
     }
 
     public void showQueue(){
         if(isEmpty()){
             System.out.println("The queue is empty!");
         }
-        for(int i = 0; i < arr.length; i++){
-            System.out.printf("arr[%d]=%d\n", i, arr[i]);
+        // 思路： 从front开始遍历，遍历多少个原始
+        //
+        for(int i = front; i < front + size(); i++){
+            System.out.printf("arr[%d]=%d\n", i % maxSize, arr[i % maxSize]);
         }
+    }
+
+    public int size(){
+        // rear = 2;
+        // front = 1;
+        // maxSize = 3;
+        return (rear + maxSize - front) % maxSize;
     }
 
     public int peekqueue(){
         if(isEmpty()){
             throw new RuntimeException("The queue is empty!");
         }
-        return arr[front+1];
+        return arr[front];
     }
 
 }
