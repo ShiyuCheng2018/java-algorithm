8b9e546f21c9befd96c5783e7107e7afd2b0317a
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-09-26 21:01:39 -0700

    feat: BinaryTree list methodes: preOrder, postOrder, infixOrder

---
+++ src/world/shiyu/tree/BinaryTreeDemo.java
@@ -0,0 +1,149 @@
+package world.shiyu.tree;
+
+import java.awt.desktop.SystemSleepEvent;
+
+public class BinaryTreeDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class BinaryTree {
+    private Node root;
+
+    public BinaryTree() {
+
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    // 前序遍历
+    public void preOrder() {
+        if (this.root != null) {
+            this.root.preOrder(); // 指向this.root
+        } else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    // 中序遍历
+    public void infixOrder() {
+        if (this.root != null) {
+            this.root.postOrder(); // 指向this.root
+        } else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    // 后序遍历
+    public void postOrder() {
+        if (this.root != null) {
+            this.root.postOrder(); // 指向this.root
+        } else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+}
+
+
+class Node {
+    private int id;
+    private String name;
+    private Node left;
+    private Node right;
+
+    public Node(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    // 编写前序遍历
+    public void preOrder() {
+        System.out.println(this); // 先输出父节点
+
+        // 递归左子树前序遍历
+        if (this.left != null) {
+            this.left.preOrder();
+        }
+
+        // 递归右子树
+        if (this.right != null) {
+            this.right.preOrder();
+        }
+    }
+
+    // 中序遍历
+    public void infixOrder() {
+        // 递归左子树遍历
+        if (this.left != null) {
+            this.left.infixOrder();
+        }
+
+        // 输出父节点
+        System.out.println(this);
+
+        // 递归右子树
+        if (this.right != null) {
+            this.right.infixOrder();
+        }
+    }
+    // 后续遍历
+
+    public void postOrder() {
+        // 递归左子树
+        if (this.left != null) {
+            this.left.postOrder();
+        }
+
+        // 递归右子树
+        if (this.right != null) {
+            this.right.postOrder();
+        }
+
+        // 输出父节点
+        System.out.println(this);
+    }
+}
